9ea2eb07a03e3f9eaa2c78b16d4df563e90093d6
Rujul Dwivedi <rujuldwivedi@icloud.com>
2024-12-25 06:32:35 +0530

    Deleted misc Files

--- media/src/main/java/com/social/media/repositories/SocialUserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.social.media.repositories;
-
-import com.social.media.models.SocialUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SocialUserRepository extends JpaRepository<SocialUser,Long> {
-}
