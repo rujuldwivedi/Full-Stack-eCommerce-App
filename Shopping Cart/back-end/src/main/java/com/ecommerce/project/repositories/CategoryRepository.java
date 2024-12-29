cc12581a4ecc941e5c3d7eb1928a5379560712f9
Rujul Dwivedi <rujuldwivedi@icloud.com>
2024-12-29 12:56:33 +0530

    deleted

--- Shopping Cart/back-end/src/main/java/com/ecommerce/project/repositories/CategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ecommerce.project.repositories;
-
-import com.ecommerce.project.model.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category,Long> {
-    Category findByCategoryName(String categoryName);
-}
