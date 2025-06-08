554ffdf839362f7dfa874f9ccb269a39ee12986b
Rujul Dwivedi <rujuldwivedi@icloud.com>
2025-06-08 17:12:25 +0530

    deleted

--- Shopping Cart/sb-ecom/src/main/java/com/ecommerce/project/payload/StripePaymentDto.java
+++
@@ -1,9 +0,0 @@
-package com.ecommerce.project.payload;
-
-import lombok.Data;
-
-@Data
-public class StripePaymentDto {
-    private Long amount;
-    private String currency;
-}
--- Shopping Cart/sb-ecom/src/main/java/com/ecommerce/project/repositories/AddressRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ecommerce.project.repositories;
-
-import com.ecommerce.project.model.Address;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AddressRepository extends JpaRepository<Address, Long> {
-}
