96bb6eae3843bb55ba38c38916a263663e0f7ed5
Rujul Dwivedi <rujuldwivedi@icloud.com>
2025-08-03 16:47:50 +0530

    App modified

--- Shopping Cart/Ecommerce-Backend/src/main/java/com/cart/ecom_proj/EcomProjApplication.java
+++ Shopping Cart/Ecommerce-Backend/src/main/java/com/cart/ecom_proj/EcomProjApplication.java
@@ -3,11 +3,19 @@ package com.cart.ecom_proj;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class EcomProjApplication {
 
 	public static void main(String[] args) {
+		// Load environment variables from .env if present (for local dev only)
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+
+		dotenv.entries().forEach(entry -> {
+		 System.setProperty(entry.getKey(), entry.getValue());
+		});
+
 		SpringApplication.run(EcomProjApplication.class, args);
 	}
-
 }
